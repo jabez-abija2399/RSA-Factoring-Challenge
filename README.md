2afd25f4266b4acd4fcc5ef08ccb1f00a270223f
jabez kifle <111049968+jabez-abija2399@users.noreply.github.com>
2022-12-29 12:34:19 +0300

    all

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Here you will find code related to Factoring & RSA Challenge
+# RSA Factoring Challenge
+
+## Algorithm #Scripting
+
+![image](https://user-images.githubusercontent.com/105078661/208537372-3171dfba-f74a-4d8f-bcb0-d02b2bd86120.png)
+
+The RSA algorithm (Rivest-Shamir-Adleman) is the basis of a cryptosystem -- a suite of cryptographic algorithms that are used for specific security services or purposes -- which enables public key encryption and is widely used to secure sensitive data, particularly when it is being sent over an insecure network such as the internet.
+
+## General Requirement
+- You can choose the language of your choice.
+- OS needs to be Standard Ubuntu 20.04 LTS/
+
+![image](https://user-images.githubusercontent.com/105078661/208537555-15cf166f-4aaa-451c-a2dc-a226918584fe.png)
--- factors.c
+++ factors.c
@@ -1,58 +1,23 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-/**
- * read_textfile- read and sent to stdout a number of charss.
- *
- *@filename: name of file
- *@letters: number of letters
- * Return: Number of printed chars.
- */
-int main(int ac, char **av)
-{
-	int i, j, flag, fd, h;
-	int leido = 0;
-	long int number, number2, k;
-	char *buf, *buf2;
-	int letters = 100;
 
-	leido = leido;
-	ac = ac;
-	number = 0;
-	fd = open(av[1], O_RDONLY);
-	buf = malloc(1000 * sizeof(char));
-	buf2 = malloc(120 * sizeof(char));
-	leido = read(fd, buf, letters);
- 	i = 0;
-	while (buf[i] != 0)
-	{
-		j = 0;
-		for (h = 0; h <= 100; h++)
-			buf2[h] = 0;
-		while (buf[i] != 10)
-			 {
-				 buf2[j] = buf[i];
-				 i++;
-				 j++;
-			 }
-		number = atol(buf2);
-		flag = 0;
-		for (k = 2; k < number /2 && flag == 0; k++)
-		{
-			if (number % k == 0)
-				flag = 1;
-		}
-		number2 = number / (k - 1);
-		printf("%li=%li*%li\n", number, number2, k - 1);
-		i++;
-	}
-	free(buf);
-	free(buf2);
-	close(fd);
-	return(0);
+int main()
+{
+    long long int num = 239809320265259;
+    long int factor1 = 2;
+    long int factor2;
+
+    while (num % factor1)
+    {
+        if (factor1 <= num)
+        {
+            factor1++;
+        }
+        else {
+            return (-1);
+        }
+    }
+
+    factor2 = num / factor1;
+    printf("%lld = %ld * %ld\n", num, factor2, factor1);
+    return (0);
 }
